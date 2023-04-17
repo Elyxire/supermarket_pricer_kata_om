adeb58009c8343933e1f146235a539c779f48752
Othman Mahfoud <mahfoud.oth@gmail.com>
2023-04-17 09:54:44 +0000

    weight and units - add converter and pricing implementations

--- src/main/java/com/kata/utils/Converter.java
+++ src/main/java/com/kata/utils/Converter.java
@@ -9,7 +9,28 @@ public class Converter {
 	}
 	
 	public static double convertToStandardUnit(double quantity, UnitEnum boughtUnit, UnitEnum standardUnit) {
-        return 0.0;
+        if (boughtUnit == standardUnit) {
+            return quantity;
+        }
+        double boughtToGramRatio = getRatio(boughtUnit);
+        double standardToGramRatio = getRatio(standardUnit);
+        return quantity * (boughtToGramRatio / standardToGramRatio);
     }
 
+    private static double getRatio(UnitEnum unit) {
+        switch (unit) {
+            case MG:
+                return 0.001;
+            case G:
+                return 1.0;
+            case KG:
+                return 1000.0;
+            case OUNCE:
+                return 28.3495;
+            case POUND:
+                return 453.592;
+            default:
+                throw new IllegalArgumentException("Invalid unit : " + unit);
+        }
+    }
 }

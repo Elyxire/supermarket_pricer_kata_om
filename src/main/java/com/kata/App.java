a87abf6cc31783785d49482f13233694e787096b
Othman Mahfoud <mahfoud.oth@gmail.com>
2023-04-17 06:40:33 +0000

    project setup

---
+++ src/main/java/com/kata/App.java
@@ -0,0 +1,16 @@
+package com.kata;
+
+import java.util.logging.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOGGER = Logger.getLogger( App.class.getName() );
+    public static void main( String[] args )
+    {
+    	LOGGER.info("Hello World!");
+    }
+}

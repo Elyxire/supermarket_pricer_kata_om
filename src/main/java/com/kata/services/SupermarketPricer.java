481e1383e8f416d68e8ebc8407ceaf558b395df5
Othman Mahfoud <mahfoud.oth@gmail.com>
2023-04-17 07:57:38 +0000

    first implementation - supermarket pricer unit test

---
+++ src/main/java/com/kata/services/SupermarketPricer.java
@@ -0,0 +1,14 @@
+package com.kata.services;
+
+import java.math.BigDecimal;
+
+import com.kata.models.Basket;
+
+import static java.math.BigDecimal.ZERO;
+
+public class SupermarketPricer {
+	public BigDecimal getDefaultTotalPrice(Basket basket) {
+		BigDecimal total = ZERO;
+        return total;
+    }
+}

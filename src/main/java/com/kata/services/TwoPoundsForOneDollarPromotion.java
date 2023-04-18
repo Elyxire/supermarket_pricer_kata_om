e6e3ba536f42c8315b749e04b9dc672c0f677797
Othman Mahfoud <mahfoud.oth@gmail.com>
2023-04-18 00:12:51 +0000

    promotion 2-pounds-for-1-dollar - add promotion implementation

--- src/main/java/com/kata/services/TwoPoundsForOneDollarPromotion.java
+++ src/main/java/com/kata/services/TwoPoundsForOneDollarPromotion.java
@@ -3,7 +3,11 @@ package com.kata.services;
 import java.math.BigDecimal;
 import java.util.Set;
 
+import com.kata.enums.UnitEnum;
 import com.kata.models.Basket;
+import com.kata.models.Item;
+import com.kata.models.UncountableItem;
+import com.kata.utils.Converter;
 
 public class TwoPoundsForOneDollarPromotion implements Promotion {
 	private static final BigDecimal PROMOTION_PRICE = new BigDecimal("1.00");
@@ -17,7 +21,27 @@ public class TwoPoundsForOneDollarPromotion implements Promotion {
     
 	@Override
 	public BigDecimal calculateDiscount(Basket basket) {
-        return BigDecimal.ZERO;
+		BigDecimal totalDiscount = BigDecimal.ZERO;
+		UncountableItem uncountableItem;
+        long numEligible;
+        double remaining;
+        double remainingInStandardUnit;
+        double boughtQuantityByPounds;
+        for (Item item : basket.getItems()) {
+            if(item instanceof UncountableItem && eligibleItemNames.contains(item.getName())) {
+            	uncountableItem = (UncountableItem) item;
+            	boughtQuantityByPounds = Converter.convertToStandardUnit(uncountableItem.getBoughtQuantity(), uncountableItem.getBoughtUnit(), UnitEnum.POUND);
+                numEligible = (long) boughtQuantityByPounds / UNIT_COUNT_PER_PROMOTION;
+                remaining = boughtQuantityByPounds % UNIT_COUNT_PER_PROMOTION;
+                remainingInStandardUnit = Converter.convertToStandardUnit(remaining, UnitEnum.POUND, uncountableItem.getStandardUnit());
+                BigDecimal subtotalDefault = uncountableItem.getPrice().multiply(BigDecimal.valueOf(Converter.convertToStandardUnit(uncountableItem.getBoughtQuantity(), uncountableItem.getBoughtUnit(), uncountableItem.getStandardUnit())));
+                BigDecimal subtotalWithDiscount = PROMOTION_PRICE.multiply(BigDecimal.valueOf(numEligible))
+                                                   .add(uncountableItem.getPrice().multiply(BigDecimal.valueOf(remainingInStandardUnit)));
+                BigDecimal discount = subtotalDefault.subtract(subtotalWithDiscount);
+                totalDiscount = totalDiscount.add(discount);
+            }
+        }
+        return totalDiscount;
 	}
     
 }
